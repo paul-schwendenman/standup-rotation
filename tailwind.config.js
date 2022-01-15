54aa0bd8bc289d434a70a2f3ae726300b437f52f
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-01-15 00:38:04 -0500

    Add tailwind and daisy config
    $ npx tailwindcss init -p

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./public/index.html', './src/**/*.svelte'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+	require('daisyui')
+  ],
+}
