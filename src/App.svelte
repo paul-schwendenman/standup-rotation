ed2c946e1c01d4849d909293fc32d4ec93ce4b6b
Paul Schwendenman <schwendenman.paul@gmail.com>
2022-01-16 22:54:58 -0500

    Add basic settings for controlling timer

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,26 @@
 <script>
   import Standup from "./components/Standup.svelte";
+  import Settings from "./components/Settings.svelte";
+  import { persistable } from "./stores";
+
+  const namesList = ["Paul", "Tyler"];
+  const showTimer = persistable('showTimer', true);
+  const duration = persistable('duration', "1:00");
 </script>
 
 <main class="mx-auto my-8 max-w-2xl">
-  <Standup />
+  <Standup {namesList} showTimer={$showTimer} time={$duration} />
+
+  <label for="my-modal-2" class="btn btn-outline modal-button">Settings</label>
+<input type="checkbox" id="my-modal-2" class="modal-toggle">
+<div class="modal">
+  <div class="modal-box">
+    <Settings {showTimer} {duration} />
+    <div class="modal-action">
+      <label for="my-modal-2" class="btn">Close</label>
+    </div>
+  </div>
+</div>
 </main>
 
 <style global>
---
+++ src/components/Settings.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { writable } from "svelte/store";
+
+	export let showTimer = writable(true);
+	export let duration = writable("1:00");
+</script>
+
+<div>
+	<label>
+		<input bind:checked={$showTimer} type="checkbox" class="toggle" />
+		Show timer
+	</label>
+
+	<label>
+		<input bind:value={$duration} type="text" class="input input-bordered" disabled={!($showTimer)} />
+		Duration
+	</label>
+</div>
+
+<style>
+	label {
+		display: block;
+	}
+</style>
