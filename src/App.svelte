e46cd42613583f2b7230356eda96ead1159efcbc
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-02-05 22:33:45 -0500

    Format project

--- src/App.svelte
+++ src/App.svelte
@@ -4,24 +4,29 @@
   import { persistable } from "./stores";
 
   const namesList = ["Paul", "Tyler"];
-  const showTimer = persistable('showTimer', true);
-  const duration = persistable('duration', "1:00");
-  const welcome = persistable('welcome', 'Welcome!');
+  const showTimer = persistable("showTimer", true);
+  const duration = persistable("duration", "1:00");
+  const welcome = persistable("welcome", "Welcome!");
 </script>
 
 <main class="mx-auto my-8 max-w-2xl">
-  <Standup {namesList} showTimer={$showTimer} time={$duration} welcome={$welcome} />
+  <Standup
+    {namesList}
+    showTimer={$showTimer}
+    time={$duration}
+    welcome={$welcome}
+  />
 
   <label for="my-modal-2" class="btn btn-outline modal-button">Settings</label>
-<input type="checkbox" id="my-modal-2" class="modal-toggle">
-<div class="modal">
-  <div class="modal-box">
-    <Settings {showTimer} {duration} {welcome} />
-    <div class="modal-action">
-      <label for="my-modal-2" class="btn">Close</label>
+  <input type="checkbox" id="my-modal-2" class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box">
+      <Settings {showTimer} {duration} {welcome} />
+      <div class="modal-action">
+        <label for="my-modal-2" class="btn">Close</label>
+      </div>
     </div>
   </div>
-</div>
 </main>
 
 <style global>
--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,32 +1,37 @@
 <script>
-	import { writable } from "svelte/store";
+  import { writable } from "svelte/store";
 
-	export let showTimer = writable(true);
-	export let duration = writable("1:00");
-	export let welcome = writable("Welcome!")
+  export let showTimer = writable(true);
+  export let duration = writable("1:00");
+  export let welcome = writable("Welcome!");
 </script>
 
 <div>
-	<label>
-		<span>Show timer:</span>
-		<input bind:checked={$showTimer} type="checkbox" class="toggle" />
-	</label>
+  <label>
+    <span>Show timer:</span>
+    <input bind:checked={$showTimer} type="checkbox" class="toggle" />
+  </label>
 
-	<label>
-		<span>Duration:</span>
-		<input bind:value={$duration} type="text" class="input input-bordered" disabled={!($showTimer)} />
-	</label>
+  <label>
+    <span>Duration:</span>
+    <input
+      bind:value={$duration}
+      type="text"
+      class="input input-bordered"
+      disabled={!$showTimer}
+    />
+  </label>
 
-	<label>
-		<span>Welcome message:</span>
-		<input bind:value={$welcome} type="text" class="input input-bordered" />
-	</label>
+  <label>
+    <span>Welcome message:</span>
+    <input bind:value={$welcome} type="text" class="input input-bordered" />
+  </label>
 </div>
 
 <style>
-	label {
-		display: flex;
-		justify-content: space-between;
-		margin-bottom: 1rem;
-	}
+  label {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
 </style>
