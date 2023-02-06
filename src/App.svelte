aabf13c860dafa1872f27a152bfbb258a70f6036
Paul Schwendenman <schwendenman.paul@gmail.com>
2023-02-05 22:32:31 -0500

    Add configurable welcome page

--- src/App.svelte
+++ src/App.svelte
@@ -6,16 +6,17 @@
   const namesList = ["Paul", "Tyler"];
   const showTimer = persistable('showTimer', true);
   const duration = persistable('duration', "1:00");
+  const welcome = persistable('welcome', 'Welcome!');
 </script>
 
 <main class="mx-auto my-8 max-w-2xl">
-  <Standup {namesList} showTimer={$showTimer} time={$duration} />
+  <Standup {namesList} showTimer={$showTimer} time={$duration} welcome={$welcome} />
 
   <label for="my-modal-2" class="btn btn-outline modal-button">Settings</label>
 <input type="checkbox" id="my-modal-2" class="modal-toggle">
 <div class="modal">
   <div class="modal-box">
-    <Settings {showTimer} {duration} />
+    <Settings {showTimer} {duration} {welcome} />
     <div class="modal-action">
       <label for="my-modal-2" class="btn">Close</label>
     </div>
--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -3,22 +3,30 @@
 
 	export let showTimer = writable(true);
 	export let duration = writable("1:00");
+	export let welcome = writable("Welcome!")
 </script>
 
 <div>
 	<label>
+		<span>Show timer:</span>
 		<input bind:checked={$showTimer} type="checkbox" class="toggle" />
-		Show timer
 	</label>
 
 	<label>
+		<span>Duration:</span>
 		<input bind:value={$duration} type="text" class="input input-bordered" disabled={!($showTimer)} />
-		Duration
+	</label>
+
+	<label>
+		<span>Welcome message:</span>
+		<input bind:value={$welcome} type="text" class="input input-bordered" />
 	</label>
 </div>
 
 <style>
 	label {
-		display: block;
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 1rem;
 	}
 </style>
