45deae5aac506f418cb5ce84275266470423d9f9
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-11-27 00:57:33 -0500

    Add App component to page route

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,83 +1,81 @@
 <script>
-  import Standup from "./components/Standup.svelte";
-  import Settings from "./components/Settings.svelte";
-  import { persistable, urlHash } from "./stores";
-  import { derived } from "svelte/store";
-  import ThemeSelector from "./components/ThemeSelector.svelte";
-  import GithubCorner from "./components/GithubCorner.svelte";
+	import Standup from './components/Standup.svelte';
+	import Settings from './components/Settings.svelte';
+	import { persistable, urlHash } from './stores';
+	import { derived } from 'svelte/store';
+	import ThemeSelector from './components/ThemeSelector.svelte';
+	import GithubCorner from './components/GithubCorner.svelte';
 
-  // const settings1 = persistable("settings", {
-  //   welcome: "Welcome!",
-  //   done: "Done.",
-  //   duration: "1:00",
-  //   showTimer: true,
-  //   names: []
-  // });
+	// const settings1 = persistable("settings", {
+	//   welcome: "Welcome!",
+	//   done: "Done.",
+	//   duration: "1:00",
+	//   showTimer: true,
+	//   names: []
+	// });
 
-  const settings = urlHash({
-    welcome: "Welcome!",
-    done: "Done.",
-    duration: "1:00",
-    showTimer: true,
-    names: [],
-  });
+	const settings = urlHash({
+		welcome: 'Welcome!',
+		done: 'Done.',
+		duration: '1:00',
+		showTimer: true,
+		names: []
+	});
 
-  const namesList = derived(
-    settings,
-    ($settings) => {
-      return $settings.names
-        .filter((item) => item.active)
-        .map((item) => item.name);
-    },
-    []
-  );
+	const namesList = derived(
+		settings,
+		($settings) => {
+			return $settings.names.filter((item) => item.active).map((item) => item.name);
+		},
+		[]
+	);
 
-  let showSettings = true;
+	let showSettings = true;
 
-  function handleRunning(event) {
-    showSettings = !showSettings;
-  }
+	function handleRunning(event) {
+		showSettings = !showSettings;
+	}
 
-  $: validNames = $namesList.length > 0;
+	$: validNames = $namesList.length > 0;
 </script>
 
 <GithubCorner />
 
 <main class="mx-auto my-8 max-w-2xl p-2">
-  <Standup
-    names={$namesList}
-    showTimer={$settings.showTimer}
-    doneMessage={$settings.done}
-    time={$settings.duration}
-    welcome={$settings.welcome}
-    on:running={handleRunning}
-  />
+	<Standup
+		names={$namesList}
+		showTimer={$settings.showTimer}
+		doneMessage={$settings.done}
+		time={$settings.duration}
+		welcome={$settings.welcome}
+		on:running={handleRunning}
+	/>
 
-  <input type="checkbox" id="settings-modal" class="modal-toggle" />
-  <div class="modal">
-    <div class="modal-box">
-      <Settings {settings} />
-      <div class="modal-action">
-        <label for="settings-modal" class="btn btn-secondary uppercase">Save</label>
-      </div>
-    </div>
-  </div>
+	<input type="checkbox" id="settings-modal" class="modal-toggle" />
+	<div class="modal">
+		<div class="modal-box">
+			<Settings {settings} />
+			<div class="modal-action">
+				<label for="settings-modal" class="btn btn-secondary uppercase">Save</label>
+			</div>
+		</div>
+	</div>
 </main>
 <footer class="fixed inset-x-0 bottom-0">
-  <div class="mx-auto max-w-2xl p-2">
-    <label
-      for="settings-modal"
-      class="btn modal-button btn-secondary uppercase"
-      class:btn-disabled={!showSettings}
-    >
-      Settings
-    </label>
-    <ThemeSelector>Theme</ThemeSelector>
-  </div>
+	<div class="mx-auto max-w-2xl p-2">
+		<label
+			for="settings-modal"
+			class="btn modal-button btn-secondary uppercase"
+			class:btn-disabled={!showSettings}
+		>
+			Settings
+		</label>
+		<ThemeSelector>Theme</ThemeSelector>
+	</div>
 </footer>
 
 <style global>
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
