a3c7b6e8cc6e6b9f70444d36dd2eb7ecb872806d
Paul Schwendenman <schwendenman.paul@gmail.com>
2024-12-01 21:36:39 -0500

    Add basic wheel

---
+++ src/lib/components/Wheel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Wheel } from "spin-wheel";
+	import { onMount } from "svelte";
+
+    export let props;
+    let wheelRef: HTMLCanvasElement;
+    let wheel;
+
+    onMount(() => {
+        wheel = new Wheel(wheelRef, props)
+    });
+
+</script>
+
+<div bind:this={wheelRef}></div>
---
+++ src/routes/wheel/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Wheel from '$lib/components/Wheel.svelte';
+import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
+    let wheelProps = {
+        name: 'Basic',
+        items: [
+            {
+                label: 'one',
+            },
+            {
+                label: 'two',
+            },
+            {
+                label: 'three',
+            },
+        ],
+    };
+</script>
+
+<Wheel props={wheelProps} />
